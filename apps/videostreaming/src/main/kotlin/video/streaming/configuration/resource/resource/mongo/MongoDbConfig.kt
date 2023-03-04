f30ed3e7162e508ad201715fb3902483e0b35d0c
changyuPark <blue_rose17@naver.com>
2023-03-04 22:44:14 +0900

    get configuration properties in application yml

--- apps/videostreaming/src/main/kotlin/video/streaming/configuration/resource/resource/mongo/MongoDbConfig.kt
+++ apps/videostreaming/src/main/kotlin/video/streaming/configuration/resource/resource/mongo/MongoDbConfig.kt
@@ -1,30 +1,37 @@
 package video.streaming.configuration.resource.resource.mongo
 
 import com.mongodb.client.MongoClients
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
 import org.springframework.data.mongodb.MongoDatabaseFactory
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
 
-
 @Configuration
 @Profile("!test")
-class MongoDbConfig() {
+class MongoDbConfig {
+    private var env: Environment? = null
+
+    @Autowired
+    fun applicationStartupEnvironment(env: Environment?) {
+        this.env = env
+    }
 
     @Bean
     fun mongoDatabaseFactory(): MongoDatabaseFactory {
         return SimpleMongoClientDatabaseFactory(
-            MongoClients.create(),
-            ""
+            MongoClients.create(env?.getProperty("spring.data.mongodb.uri") ?: ""),
+            env?.getProperty("spring.data.mongodb.database") ?: ""
         )
     }
 
     @Bean
     fun mongoTemplate(): MongoTemplate {
-        return MongoTemplate(mongoDatabaseFactory())
+        return MongoTemplate(
+            mongoDatabaseFactory()
+        )
     }
-
 }
